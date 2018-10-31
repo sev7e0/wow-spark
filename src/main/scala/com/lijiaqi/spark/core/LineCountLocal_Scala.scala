dff8a86b4bea8bae851817e2965c96f2123a3815
sev7e0 <sev7e0@gmail.com>
2018-10-31 17:33:32 +0800

    :camel: change

--- src/main/scala/com/lijiaqi/spark/core/LineCountLocal_Scala.scala
+++ src/main/scala/com/lijiaqi/spark/core/LineCountLocal_Scala.scala
@@ -1,21 +1,25 @@
 package com.lijiaqi.spark.core
 
-import org.apache.spark.sql.SparkSession
+import org.apache.spark._
 
 object LineCountLocal_Scala extends AutoCloseable{
 
-  private val session: SparkSession = SparkSession.builder().appName("LineCountLocal_Scala").master("local").getOrCreate()
+//  private val session: SparkSession = SparkSession.builder()
+//    .appName("LineCountLocal_Scala")
+//    .master("local")
+//    .getOrCreate()
+
 
   def main(args: Array[String]): Unit = {
+    val conf = new SparkConf().setAppName("LineCountLocal_Scala").setMaster("local")
+    val sc = new SparkContext(conf)
 
-    val context = session.sparkContext
-
-    val lines = context.textFile("file:/home/sev7e0/bigdata/spark-2.2.0-bin-hadoop2.7/README.md")
-    val res = lines.map(line => line.length).reduce(_+_)
-    println(s"res = ${res}")
+    val lines = sc.textFile("file:/home/sev7e0/bigdata/spark-2.2.0-bin-hadoop2.7/README.md")
+    val res = lines.map(line=>line.length).reduce(_+_)
+    println(s"res = $res")
   }
 
   override def close(): Unit = {
-    session.close()
+//    session.close()
   }
 }

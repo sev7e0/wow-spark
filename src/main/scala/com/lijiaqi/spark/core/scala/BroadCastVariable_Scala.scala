9c3d128b308ef5b2bb5cfbd182a347aba9371aaf
sev7e0 <sev7e0@163.com>
2018-11-04 23:23:55 +0800

    :cat: BroadCastVariable实战

---
+++ src/main/scala/com/lijiaqi/spark/core/scala/BroadCastVariable_Scala.scala
@@ -0,0 +1,19 @@
+package com.lijiaqi.spark.core.scala
+
+import org.apache.spark._
+//BroadCast Scala版本
+class BroadCastVariable_Scala {
+
+  def main(args: Array[String]): Unit = {
+    val conf = new SparkConf()
+    conf.setAppName("BroadCastVariable_Scala").setMaster("local")
+    val context = new SparkContext(conf)
+
+    val array = Array(1,2,3,4,5,6,7,8)
+    val factor = 5
+    //设置共享变量
+    val broadcast = context.broadcast(factor)
+    context.parallelize(array).map(arr=>arr*broadcast.value).foreach(args=>println(args))
+  }
+
+}

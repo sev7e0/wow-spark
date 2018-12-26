f29cb90e160f6cc4d093d3ca0a8c440dd91e30b3
sev7e0 <sev7e0@163.com>
2018-12-26 17:16:35 +0800

    :floppy_disk: Something about Merge Sort

--- src/main/scala/com/lijiaqi/spark/algorithm/MergeSort.java
+++ src/main/scala/com/lijiaqi/spark/algorithm/MergeSort.java
@@ -7,6 +7,17 @@ package com.lijiaqi.spark.algorithm;
  * @create: 2018-12-26 14:35
  **/
 
+/**
+ * 中心思想:它是一种简单直观的排序算法。它的工作原理如下。首先在未排序序列中找到最小（大）元素，存放到排序序列的起始位置，然后，再从剩余未排序元素中继续寻找最小（大）元素，然后放到已排序序列的末尾。
+ *          以此类推，直到所有元素均排序完毕。
+ *          它的主要优点与数据移动有关。如果某个元素位于正确的最终位置上，则它不会被移动。选择排序每次交换一对元素，它们当中至少有一个将被移到其最终位置上，因此对n个元素的表进行排序总共进行至多n-1次交换。
+ *          在所有的完全依靠交换去移动元素的排序方法中，选择排序属于非常好的一种
+ * 归并优化:对于小数组可以采用和快排一样的方式,使用插入排序或者选择排序.
+ * 说明:归并排序的时间复杂度是O(NLogN)，空间复杂度是O(N)。
+ *      辅助数组是一个共用的数组。如果在每个归并的过程中都申请一个临时数组会造成比较大的时间开销。
+ *      归并的过程需要将元素复制到辅助数组，再从辅助数组排序复制回原数组，会拖慢排序速度。
+ */
+
 public class MergeSort {
 
     public static void main(String[] args) {
@@ -23,7 +34,7 @@ public class MergeSort {
 
     private void mergeSort(int[] arr, int[] temp, int first, int last){
         if (first<last){
-            int mid = (first+last)/2;
+            int mid = (first+last)>>1;
             mergeSort(arr, temp, first, mid);
             mergeSort(arr, temp, mid+1, last);
             sort(arr, temp, first, mid, last);

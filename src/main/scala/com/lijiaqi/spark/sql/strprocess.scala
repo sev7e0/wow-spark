5643b4a8b561975027814a15f89be14e9cc806e9
sev7e0 <sev7e0@163.com>
2019-01-15 00:59:21 +0800

    :ant: SparkSQl json practice

--- src/main/scala/com/lijiaqi/spark/sql/strprocess.scala
+++ src/main/scala/com/lijiaqi/spark/sql/strprocess.scala
@@ -7,7 +7,7 @@ object strprocess {
   def main(args: Array[String]): Unit = {
     val session = SparkSession.builder().appName("strprocess").master("local").getOrCreate()
 
-    val strRdd = session.sparkContext.textFile("C:\\Users\\lijiaqi-yd\\Desktop\\ZZ-FJMX-1.txt")
+    val strRdd = session.sparkContext.textFile("\\Users\\lijiaqi-yd\\Desktop\\ZZ-FJMX-1.txt")
 
     strRdd.flatMap(str => str.split(" ")).collect().foreach(str => if(str!= "") print("\""+str.trim+"\","))
   }

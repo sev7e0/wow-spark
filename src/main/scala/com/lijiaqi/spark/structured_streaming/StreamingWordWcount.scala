2be05736df0fff76dfc299c3714c10ec83f68d2c
sev7e0 <sev7e0@163.com>
2019-01-22 16:15:22 +0800

    :ant: StreamingWordWcount

---
+++ src/main/scala/com/lijiaqi/spark/structured_streaming/StreamingWordWcount.scala
@@ -0,0 +1,37 @@
+package com.lijiaqi.spark.structured_streaming
+
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions._
+
+object StreamingWordWcount {
+
+  def main(args: Array[String]): Unit = {
+    val spark = SparkSession.builder()
+      .master("local")
+      .appName("StreamingWordWcount")
+      .getOrCreate()
+
+    import spark.implicits._
+
+    //输入表
+    val line = spark
+      .readStream
+      .format("socket")
+      .option("host", "172.23.7.72")
+      .option("port", 9999)
+      .load()
+
+    val word = line.as[String].flatMap(_.split(" "))
+
+    //输出表
+    val count = word.groupBy("value").count()
+
+    val query = count.writeStream
+      .outputMode(outputMode = "complete")
+      .format("console")
+      .start()
+
+    query.awaitTermination()
+  }
+
+}
